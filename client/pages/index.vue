31f3e34006069ce8323d52496f1d25b4755ae217
Bektur <prostreyd@gmail.com>
2022-12-15 22:43:19 +0600

    first commit

---
+++ client/pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>index page</div>
+
+    <button @click="count++">+</button>
+    <div>{{ count }}</div>
+    <button @click="count--">-</button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const count = useState<number>(() => 1);
+</script>
